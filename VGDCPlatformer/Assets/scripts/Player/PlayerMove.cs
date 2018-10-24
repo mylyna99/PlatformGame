ce05f59985f14dc913b3c172d8186c3cf9597126
Richard Hsu <shengwh1@uci.edu>
2018-10-23 23:08:25 -0700

    Player going the right way

--- VGDCPlatformer/Assets/scripts/Player/PlayerMove.cs
+++ VGDCPlatformer/Assets/scripts/Player/PlayerMove.cs
@@ -17,7 +17,7 @@ public class PlayerMove : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        horizontalMove = runSpeed;
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
@@ -26,7 +26,7 @@ public class PlayerMove : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        controller.Move(runSpeed * Time.fixedDeltaTime, jump);
         jump = false;
     }
 
