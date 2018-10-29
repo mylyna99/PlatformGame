32272db7e934b411a6ee81784090efd21c4109eb
jjlebow@gmail.com <jlebow@uci.edu>
2018-10-28 19:09:20 -0700

    input music file

---
+++ VGDCPlatformer/Assets/scripts/AudioObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioObject : MonoBehaviour {
+
+    public AudioClip MusicClip;
+
+    public AudioSource MusicSource;
+	// Use this for initialization
+	void Start () {
+        MusicSource.clip = MusicClip;
+        MusicSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
