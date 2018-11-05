97d3d190e547af313b380bba1a97e0fa72b0a0b6
michacg <michacg2@uci.edu>
2018-11-04 23:51:43 -0800

    Added component and script to make the level switch. Need to add more to script so that the collider recognizes player contact to initiate next level

---
+++ VGDCPlatformer/Assets/SwitchLevels.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevels : MonoBehaviour {
+
+	void OpenLevel(Collider other)
+	{
+		SceneManager.LoadScene("FallenLevelScene"); //change level to our game
+	}
+}
