1b5376eeac0fd03b3f2d1123a565038eda13febf
michacg <michacg2@uci.edu>
2018-11-27 18:52:52 -0800

    New file AudioObjectFirstLevel.cs

---
+++ VGDCPlatformer/Assets/scripts/AudioObjectFirstLevel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioObject : MonoBehaviour {
+
+    public AudioClip MusicClip;
+
+    public AudioSource MusicSource;
+	// Use this for initialization
+	void Start () {
+        MusicSource.clip = MusicClip;
+        MusicSource.Play(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
